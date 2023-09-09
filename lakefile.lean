147b7d792cbec7a865436e972ce8606fb3782b15
tydeu <tydeu@hatpress.net>
2023-09-09 15:37:42 -0400

    test: verify basic test output via `lake run test`

--- lakefile.lean
+++ lakefile.lean
@@ -5,3 +5,20 @@ package itertools
 
 @[default_target]
 lean_lib Itertools
+
+script test do
+  let testFile : FilePath := "test" / "basic.lean"
+  let eOutFile := testFile.withExtension "expected.out"
+  let eOut ← IO.FS.readFile eOutFile
+  let out ← IO.Process.output {
+    cmd := (← getLean).toString, args := #[testFile.toString], env := ← getAugmentedEnv
+  }
+  let pOut := out.stderr ++ out.stdout
+  let pOutFile := testFile.withExtension "produced.out"
+  IO.FS.writeFile pOutFile pOut
+  if eOut = pOut then
+    IO.eprintln s!"{testFile}: output matched"
+    return 0
+  else
+    IO.eprintln s!"{testFile}: produced output did not match expected output"
+    return 1
